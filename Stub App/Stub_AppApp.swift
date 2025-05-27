6e8aa743147c1dd9f4b8d1367d48a59deb1c48a0
Elzh@n <elzhan-96@mail.ru>
2025-05-27 19:40:33 +0500

    Initial Commit

---
+++ Stub App/Stub_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stub_AppApp.swift
+//  Stub App
+//
+//  Created by elzhankk on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Stub_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
